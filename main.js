a090c0d725bda07ab62122490fbd88c0e0d0c5c6
moneeb <moneebalalfi@gmail.com>
2022-08-14 23:25:08 +0300

    feat: adding shadows

--- main.js
+++ main.js
@@ -183,6 +183,7 @@ for (let i = 0; i < 50; i++) {
 
   grave.rotation.y = (Math.random() - 0.5) * 0.4;
   grave.rotation.z = (Math.random() - 0.5) * 0.4;
+  grave.castShadow = true;
 
   graves.add(grave);
 }
@@ -292,6 +293,23 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 renderer.setClearColor("#415a77");
+
+// Shadows
+renderer.shadowMap.enabled = true;
+
+moonLight.castShadow = true;
+doorLight.castShadow = true;
+ghost1.castShadow = true;
+ghost2.castShadow = true;
+ghost3.castShadow = true;
+
+walls.castShadow = true;
+bush1.castShadow = true;
+bush2.castShadow = true;
+bush3.castShadow = true;
+bush4.castShadow = true;
+
+floor.receiveShadow = true;
 
 /**
  * Animate
