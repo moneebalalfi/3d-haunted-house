1bcf12d4e50210e2dafd5f014f00006322b7112f
moneeb <moneebalalfi@gmail.com>
2022-08-14 22:14:28 +0300

    feat: position objects

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as dat from "dat.gui";
+import { Mesh } from "three";
 
 document.querySelector("#app").innerHTML = `
   <canvas id="webgl"></canvas>
@@ -12,6 +13,7 @@ document.querySelector("#app").innerHTML = `
  */
 // Debug
 const gui = new dat.GUI();
+gui.closed = true;
 
 // Canvas
 const canvas = document.querySelector("#webgl");
@@ -28,6 +30,85 @@ const textureLoader = new THREE.TextureLoader();
  * House
  */
 
+// Group
+const house = new THREE.Group();
+scene.add(house);
+
+// Walls
+const walls = new THREE.Mesh(
+  new THREE.BoxBufferGeometry(4, 2.5, 4),
+  new THREE.MeshStandardMaterial({
+    color: "#A57F60",
+  })
+);
+walls.position.y = 2.5 / 2;
+house.add(walls);
+
+// Roof - Pyramid (Cone with 4 segments)
+const roof = new THREE.Mesh(
+  new THREE.ConeBufferGeometry(3.5, 1, 4),
+  new THREE.MeshStandardMaterial({ color: "#DB5A42" })
+);
+roof.position.y = 2.5 + 0.5;
+roof.rotation.y = Math.PI * 0.25;
+house.add(roof);
+
+// Door
+const door = new THREE.Mesh(
+  new THREE.PlaneBufferGeometry(2, 2),
+  new THREE.MeshStandardMaterial({ color: "#E8AE68" })
+);
+
+door.position.y = 1;
+door.position.z = 4 / 2 + 0.01;
+house.add(door);
+
+// Bushes
+const bushGeometry = new THREE.SphereBufferGeometry(1, 16, 16);
+const bushMaterial = new THREE.MeshStandardMaterial({ color: "#80ed99" });
+
+const bush1 = new Mesh(bushGeometry, bushMaterial);
+const bush2 = new Mesh(bushGeometry, bushMaterial);
+const bush3 = new Mesh(bushGeometry, bushMaterial);
+const bush4 = new Mesh(bushGeometry, bushMaterial);
+
+bush1.scale.set(0.5, 0.5, 0.5);
+bush1.position.set(0.8, 0.2, 2.2);
+
+bush2.scale.set(0.25, 0.25, 0.25);
+bush2.position.set(1.4, 0.1, 2.1);
+
+bush3.scale.set(0.35, 0.35, 0.35);
+bush3.position.set(-1.3, 0.2, 2.1);
+
+bush4.scale.set(0.25, 0.25, 0.25);
+bush4.position.set(-1.7, 0.1, 2);
+
+scene.add(bush1, bush2, bush3, bush4);
+
+/* Group */
+
+const graves = new THREE.Group();
+scene.add(graves);
+
+const graveGeometry = new THREE.BoxBufferGeometry(0.6, 0.8, 0.2);
+const graveMaterial = new THREE.MeshStandardMaterial();
+
+for (let i = 0; i < 50; i++) {
+  const angle = Math.random() * Math.PI * 2;
+  const radius = 3 + Math.random() * 6;
+  const x = Math.sin(angle) * radius;
+  const z = Math.cos(angle) * radius;
+
+  const grave = new THREE.Mesh(graveGeometry, graveMaterial);
+  grave.position.set(x, 0.3, z);
+
+  grave.rotation.y = (Math.random() - 0.5) * 0.4;
+  grave.rotation.z = (Math.random() - 0.5) * 0.4;
+
+  graves.add(grave);
+}
+
 // Floor
 const floor = new THREE.Mesh(
   new THREE.PlaneBufferGeometry(20, 20),
@@ -41,18 +122,27 @@ scene.add(floor);
  * Lights
  */
 // Ambient light
-const ambientLight = new THREE.AmbientLight("#ffffff", 0.5);
+const ambientLight = new THREE.AmbientLight("#3d405b", 0.4);
 gui.add(ambientLight, "intensity").min(0).max(1).step(0.001);
 scene.add(ambientLight);
 
 // Directional light
-const moonLight = new THREE.DirectionalLight("#ffffff", 0.5);
+const moonLight = new THREE.DirectionalLight("#3d405b", 0.4);
 moonLight.position.set(4, 5, -2);
 gui.add(moonLight, "intensity").min(0).max(1).step(0.001);
 gui.add(moonLight.position, "x").min(-5).max(5).step(0.001);
 gui.add(moonLight.position, "y").min(-5).max(5).step(0.001);
 gui.add(moonLight.position, "z").min(-5).max(5).step(0.001);
 scene.add(moonLight);
+
+// Door Light
+const doorLight = new THREE.PointLight("#ff7d46", 1, 7);
+doorLight.position.set(0, 2.2, 2.7);
+house.add(doorLight);
+
+// Fog
+const fog = new THREE.Fog("#415a77", 1, 15);
+scene.fog = fog;
 
 /**
  * Sizes
@@ -103,6 +193,7 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.setClearColor("#415a77");
 
 /**
  * Animate
